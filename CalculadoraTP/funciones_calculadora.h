10c62a8a6ece982fcc970ab99ff914f520a1ec33
Luciano S <44885834+attrix182@users.noreply.github.com>
2019-04-04 12:16:55 -0300

    Add files via upload

--- CalculadoraTP/funciones_calculadora.h
+++ CalculadoraTP/funciones_calculadora.h
@@ -1,9 +1,47 @@
-float sumar (float numero1, float numero2);
+/** \brief
+ *
+ * \param numero1 float
+ * \param numero2 float
+ * \return float
+ *
+ */
+float sumar(float numero1, float numero2);
 
-float restar (float numero1, float numero2);
 
-float multiplicar (float numero1, float numero2);
+/** \brief
+ *
+ * \param numero1 float
+ * \param numero2 float
+ * \return float
+ *
+ */
+float restar(float numero1, float numero2);
 
-float dividir (float numero1, float numero2);
 
-double factorial (double numero);
+/** \brief
+ *
+ * \param numero1 float
+ * \param numero2 float
+ * \return float
+ *
+ */
+float multiplicar(float numero1, float numero2);
+
+
+/** \brief
+ *
+ * \param numero1 float
+ * \param numero2 float
+ * \return float
+ *
+ */
+float dividir(float numero1, float numero2);
+
+
+/** \brief
+ *
+ * \param numero double
+ * \return double
+ *
+ */
+double factorial(double numero);
